773614bcf20bbac230233d8afa0e31e2fc473466
Carlos Rocha <carlosrocha1896@gmail.com>
2024-08-27 17:30:09 -0300

    implementando PWA

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+const cacheName = 'clima-agora-v1';
+const assetsToCache = [
+  '/',
+  '/index.html',
+  '/styles/style.css',
+  '/script/script.js',
+  '/images/icons/icon-192x192.png',
+  '/images/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(assetsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [cacheName];
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
