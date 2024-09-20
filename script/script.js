82f64e9d982dbc2003d41a1af71c8e8423637d88
Carlos Rocha <carlosrocha1896@gmail.com>
2024-09-19 22:09:15 -0300

    acessando geolocalizacao do usuario

--- script/script.js
+++ script/script.js
@@ -148,3 +148,22 @@ suggestionButtons.forEach((btn) => {
     showWeatherData(city);
   });
 });
+
+
+window.onload = function() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(success, error);
+  } else {
+    alert("Geolocation is not supported by this browser.");
+  }
+};
+
+function success(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  console.log(`Latitude: ${lat}, Longitude: ${lon}`);
+}
+
+function error() {
+  alert("Não foi possível obter sua localização.");
+}
